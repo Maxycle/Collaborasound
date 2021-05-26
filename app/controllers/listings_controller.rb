73c707b6a04455d7ea8f5f8fe2c2f1c4f68a9ccc
maxime seiter <maxycle@hotmail.com>
2021-05-26 20:33:23 +0100

    PG_search added

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,37 @@
 class ListingsController < ApplicationController
 
   def index
-    @listings = Listing.all
+
+    @listings = Listing.all.sort_by { |listing| listing.created_at }.reverse!
+
+    if params[:instrument].present?
+      @listings = Listing.where("instrument ILIKE? ", "%#{params[:instrument]}%")
+      if params[:style].present? 
+        @listings = @listings.where("style ILIKE? ", "%#{params[:style]}%")
+        if params[:location].present?
+            @listings = @listings.where("location ILIKE? ", "%#{params[:location]}%")
+            if params[:band].present?
+              @listings = @listings.where("band ILIKE? ", "%#{params[:band]}%")
+            end
+        end
+      end
+    elsif params[:style].present?
+      @listings = Listing.where("style ILIKE? ", "%#{params[:style]}%")
+        if params[:location].present?
+            @listings = @listings.where("location ILIKE? ", "%#{params[:location]}%")
+            if params[:band].present?
+              @listings = @listings.where("band ILIKE? ", "%#{params[:band]}%")
+            end
+        end
+      elsif if params[:location].present?
+        @listings = Listing.where("location ILIKE? ", "%#{params[:location]}%")
+          if params[:band].present?
+            @listings = @listings.where("band ILIKE? ", "%#{params[:band]}%")
+          end
+        elsif params[:band].present?
+          @listings = Listing.where("band ILIKE? ", "%#{params[:band]}%")
+        end
+    end
   end 
 
   def show
@@ -14,13 +44,19 @@ class ListingsController < ApplicationController
  
   def create
     @listing = Listing.new(listing_params)
-    @listing.save
-    # Will raise ActiveModel::ForbiddenAttributesError
+    @listing.user = current_user
+    # @listing.user.first_name = current_user.first_name
+    if @listing.save
+      redirect_to listings_path
+    else
+      flash.alert = "Please check your form for any errors and try again."
+      render :new
+    end
   end
 
   private
 
   def listing_params
-    params.require(:listing).permit(:style, :instrument)
+    params.require(:listing).permit(:style, :instrument, :band, :location)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,25 @@
 class Listing < ApplicationRecord
     belongs_to :user
+
+      include PgSearch::Model
+      pg_search_scope :search_by_instrument,
+        against: [ :instrument ],
+        using: {
+          tsearch: { prefix: true }
+        }
+      pg_search_scope :search_by_band,
+      against: [ :band ],
+      using: {
+        tsearch: { prefix: true }
+      }
+      pg_search_scope :search_by_location,
+      against: [ :location ],
+      using: {
+        tsearch: { prefix: true }
+      }
+      pg_search_scope :search_by_style,
+      against: [ :style ],
+      using: {
+        tsearch: { prefix: true }
+      }
 end
