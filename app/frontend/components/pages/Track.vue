59acf29624fb5a9ec29facefede03b3a58c4090b
Maxycle <maxycle@hotmail.com>
2024-02-26 18:47:03 +0100

    fix: fix data sent by getters

--- app/frontend/components/pages/Track.vue
+++ app/frontend/components/pages/Track.vue
@@ -1,7 +1,7 @@
 <template>
 	<Container>
 		<div class="w-4/5">
-			<div>{{ author }} in {{ band }} needs some {{ instrument }} to play some {{ genre }}</div>
+			<div class="pb-2">{{ author }} in {{ band }} needs some {{ instrument }} to play some {{ genre }}</div>
 			<div class="flex justify-center space-x-4 pb-4">
 				<div class="border-2 border-red-400 bg-green-300 rounded-2xl p-4">listen to ze Track</div>
 				<div class="border-2 border-red-400 bg-green-300 rounded-2xl p-4">download ze track</div>
@@ -34,13 +34,13 @@ export default {
 	computed: {
 		...mapGetters(['trackBasicData']),
 		genre() {
-      return this.trackBasicData.music_genre;
+      return this.trackBasicData.music_genres.map(item => item.name).join('/ ')
     },
     author() {
       return this.trackBasicData.author;
     },
     instrument() {
-      return this.trackBasicData.instrument;
+      return this.trackBasicData.instruments.map(item => item.name).join('/ ')
     },
     band() {
       return this.trackBasicData.band;
