7838aac1af1df21e2092a7a08c1e10dc3aef5237
Maxycle <maxycle@hotmail.com>
2024-02-26 01:05:53 +0100

    feat: add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  content: [
-    './app/views/**/*.{html,html.erb,erb}',
-    './app/views/devise/**/*.{html,html.erb,erb}',
-    './app/frontend/components/**/*.{vue,js,ts,jsx,tsx}',
-    './app/frontend/**/*.{vue,js,ts,jsx,tsx}',
-    './app/**/*.{vue,js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./app/views/**/*.{html,html.erb,erb}',
+		'./app/views/devise/**/*.{html,html.erb,erb}',
+		'./app/frontend/components/**/*.{vue,js,ts,jsx,tsx}',
+		'./app/frontend/**/*.{vue,js,ts,jsx,tsx}',
+		'./app/**/*.{vue,js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				'sans': ['Kode Mono', 'sans-serif']
+			}
+		},
+	},
+	plugins: [],
 }
