36986da68cd67e49e8a949eb7ceee060934d2a88
Maxycle <maxycle@hotmail.com>
2024-02-28 18:32:11 +0100

    feat: add NavBar and map

---
+++ app/frontend/components/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="flex justify-end space-x-2 bg-blue-300 pr-4">
+		<button :class="{ 'bg-blue-600': activeButton === 'home' }"
+			class="p-2 hover:bg-red-400 transition-colors duration-300 hover:font-bold rounded-full"><router-link to="/"
+				@click="setActiveButton('home')">home</router-link></button>
+		<button :class="{ 'bg-blue-600': activeButton === 'myTracks' }"
+			class="p-2 hover:bg-red-400 transition-colors duration-300 hover:font-bold rounded-full"><router-link
+				to="/my_tracks" @click="setActiveButton('myTracks')">My tracks</router-link></button>
+		<button class="p-2 hover:bg-red-400 transition-colors duration-300 hover:font-bold rounded-full"
+			@click="redirectToEditProfile">Edit profile</button>
+		<button class="p-2 hover:bg-red-400 transition-colors duration-300 hover:font-bold rounded-full"
+			@click="logout">Logout</button>
+	</div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+	data() {
+		return {
+			activeButton: ''
+		}
+	},
+
+	mounted() {
+		this.setActiveButton(this.$route.path);
+	},
+
+	computed: {
+		setActiveButton() {
+			return (routePath) => {
+				switch (routePath) {
+					case '/':
+						this.activeButton = 'home';
+						break;
+					case '/my_tracks':
+						this.activeButton = 'myTracks';
+						break;
+					case '/users/edit':
+						this.activeButton = 'editProfile';
+						break;
+					default:
+						this.activeButton = '';
+				}
+			}
+		}
+	},
+
+	methods: {
+		async logout() {
+			try {
+				await axios.delete('/users/sign_out')
+				window.location.href = '/'
+			} catch (error) {
+				console.error('Error logging out', error)
+			}
+		},
+		redirectToEditProfile() {
+			window.location.href = '/users/edit'
+		}
+	}
+}
+</script>
--- app/frontend/components/containers/Container.vue
+++ app/frontend/components/containers/Container.vue
@@ -1,5 +1,49 @@
 <template>
-	<div class="flex items-center justify-center py-4 bg-orange-100 w-full">
-		<slot />
+	<NavBar />
+	<div class="w-full">
+		<div class="flex flex-col items-center py-4 bg-orange-100">
+			<button v-if="!isModalOpen" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
+				@click="openModal">{{ mapButtonText }}</button>
+			<button v-else class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
+				@click="closeModal">{{ mapButtonText }}</button>
+			<div v-if="isModalOpen" class="w-4/6 mb-2">
+				<div class="">
+					<button class="close" @click="closeModal">&times;</button>
+					<Map class="w-full" />
+				</div>
+			</div>
+			<slot />
+		</div>
 	</div>
 </template>
+
+<script>
+import NavBar from '../NavBar.vue'
+import Map from '../map/Map.vue'
+export default {
+	components: {
+		NavBar,
+		Map
+	},
+
+	data() {
+		return {
+			isModalOpen: false
+		};
+	},
+	computed: {
+		mapButtonText() {
+			return this.isModalOpen ? 'Close da muzzakuckin map' : 'See on da muzzakuckin map'
+		}
+	},
+
+	methods: {
+		openModal() {
+			this.isModalOpen = true;
+		},
+		closeModal() {
+			this.isModalOpen = false;
+		}
+	}
+}
+</script>
