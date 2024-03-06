ad8197e6309a67f361e55034c56ea9ea7dff843e
Maxycle <maxycle@hotmail.com>
2024-03-06 16:41:02 +0100

    fix: add condition to show map button

--- app/frontend/components/containers/Container.vue
+++ app/frontend/components/containers/Container.vue
@@ -1,11 +1,9 @@
 <template>
 	<NavBar />
 	<div class="w-full">
-		<div class="flex flex-col items-center py-4 bg-orange-100">
-			<button v-if="!isModalOpen" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
-				@click="openModal">{{ mapButtonText }}</button>
-			<button v-else class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
-				@click="closeModal">{{ mapButtonText }}</button>
+		<div class="flex flex-col items-center py-4 bg-zinc-200">
+			<button v-if="$route.path === '/'" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
+				@click="toggleModal">{{ mapButtonText }}</button>
 			<div v-if="isModalOpen" class="w-4/6 transition ease-in-out duration-1000 opacity-100 mb-2">
 				<div class="">
 					<Map class="w-full" />
@@ -37,11 +35,8 @@ export default {
 	},
 
 	methods: {
-		openModal() {
-			this.isModalOpen = true;
-		},
-		closeModal() {
-			this.isModalOpen = false;
+		toggleModal() {
+			this.isModalOpen = !this.isModalOpen
 		}
 	}
 }
