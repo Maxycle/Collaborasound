4fb814b870559dcff22b1965bdb985e98fcd35d4
Maxycle <maxycle@hotmail.com>
2024-03-11 19:29:21 +0100

    fix: styling

--- app/frontend/components/pages/Track.vue
+++ app/frontend/components/pages/Track.vue
@@ -1,17 +1,22 @@
 <template>
-	<Container>
-		<div class="w-4/5">
+	<Container class="overflow-y-auto">
+		<div class="w-4/5 flex flex-col min-h-full">
 			<div class="pb-4"><span class="font-bold">{{ author }}</span> needs some <span class="font-bold">{{ instrument ||
-				'stuff' }}</span> to make some <span class="font-bold">{{ genre || 'weird' }}</span> muzik for the song :</div>
+				'stuff' }}</span> to make some <span class="font-bold">{{ genre || 'weird' }}</span> muzik for the song :
+			</div>
 			<h1 class="flex justify-center pb-4"> "{{ trackTitle }}""</h1>
 			<div class="flex justify-center space-x-4 pb-4">
-				<AnarcapButton class="transition hover:text-red-500 hover:font-extrabold hover:-translate-y-1 hover:scale-110 duration-300"><span class="relative">listen to "{{ trackTitle
-				}}"</span></AnarcapButton>
-				<AnarcapButton class="transition hover:text-red-500 hover:font-extrabold hover:-translate-y-1 hover:scale-110 duration-300"><span class="relative">download "{{ trackTitle
-				}}""</span></AnarcapButton>
-				<AnarcapButton class="transition hover:text-red-500 hover:font-extrabold hover:-translate-y-1 hover:scale-110 duration-300"><span class="relative"><router-link
-							:to="{ name: 'result_track', params: { zeTrackId: trackId } }">upload your
-							track/result</router-link></span></AnarcapButton>
+				<AnarcapButton class="transition hover:-translate-x-6 hover:scale-110 duration-300 shadow-lg shadow-zinc-600">
+					<span class="relative">listen to "{{ trackTitle
+						}}"</span>
+				</AnarcapButton>
+				<AnarcapButton class="transition hover:z-10 hover:scale-110 duration-300 shadow-lg shadow-zinc-600"><span
+						class="relative">download "{{ trackTitle
+						}}"</span></AnarcapButton>
+				<AnarcapButton class="transition hover:translate-x-6 hover:scale-110 duration-300 shadow-lg shadow-zinc-600">
+					<span class="relative"><router-link :to="{ name: 'result_track', params: { zeTrackId: trackId } }">upload your
+							track/result</router-link></span>
+				</AnarcapButton>
 			</div>
 			<div class="flex justify-center">
 				<TracksList isResult :trackId="trackId" class="justify-center" />
