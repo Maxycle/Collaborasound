341b74418f3b4ce1b0cea5cb32f0dfe90e5041ae
Maxycle <maxycle@hotmail.com>
2024-03-21 16:51:06 +0100

    fix: add welcome message to navbar

--- app/frontend/components/NavBar.vue
+++ app/frontend/components/NavBar.vue
@@ -1,27 +1,35 @@
 <template>
-	<div class="flex justify-end space-x-2 bg-zinc-200 pr-4 shadow-lg shadow-stone-400">
-		<NavBarButton :isActive="$route.path === '/'">
-			<span class="relative"><router-link to="/">Home</router-link></span>
-		</NavBarButton>
-		<NavBarButton :isActive="$route.path === '/my_own_tracks'">
-			<span class="relative"><router-link to="/my_own_tracks">My tracks</router-link></span>
-		</NavBarButton>
-		<NavBarButton @click="redirectToEditProfile">
-			<span class="relative">Edit profile</span>
-		</NavBarButton>
-		<NavBarButton @click="logout">
-			<span class="relative">Logout</span>
-		</NavBarButton>
+	<div class="flex justify-between px-4 items-center bg-zinc-200 shadow-lg shadow-stone-400">
+		<div>Hello {{ loggedInUser.first_name }} you muzzafucka !!</div>
+		<div class="flex justify-end space-x-2">
+			<NavBarButton :isActive="$route.path === '/'">
+				<span class="relative"><router-link to="/">Home</router-link></span>
+			</NavBarButton>
+			<NavBarButton :isActive="$route.path === '/my_own_tracks'">
+				<span class="relative"><router-link to="/my_own_tracks">My tracks</router-link></span>
+			</NavBarButton>
+			<NavBarButton @click="redirectToEditProfile">
+				<span class="relative">Edit profile</span>
+			</NavBarButton>
+			<NavBarButton @click="logout">
+				<span class="relative">Logout</span>
+			</NavBarButton>	
+		</div>
 	</div>
 </template>
 
 <script>
 import axios from 'axios'
 import NavBarButton from './buttons/NavBarButton.vue'
+import { mapGetters } from 'vuex';
 
 export default {
 	components: {
 		NavBarButton
+	},
+
+	computed: {
+		...mapGetters(['loggedInUser']),
 	},
 
 	methods: {
